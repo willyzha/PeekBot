a81ae2aa80e3e8fd10bed1824f5c2d6506ae77e9
Willy <willyzha@gmail.com>
2017-10-08 00:25:09 -0700

    Test TTS join voice channel

--- cogs/tts.py
+++ cogs/tts.py
@@ -40,9 +40,62 @@ class TextToSpeech:
     async def on(self, ctx):
         """Turn on TextToSpeech"""
         server = ctx.message.server
+        
+        if self.is_playing(server):
+            await ctx.invoke(self._queue, url=url)
+            return  # Default to queue
+        
+                # Checking already connected, will join if not
+
+        try:
+            self.has_connect_perm(author, server)
+        except AuthorNotConnected:
+            await self.bot.say("You must join a voice channel before I can"
+                               " play anything.")
+            return
+        except UnauthorizedConnect:
+            await self.bot.say("I don't have permissions to join your"
+                               " voice channel.")
+            return
+        except UnauthorizedSpeak:
+            await self.bot.say("I don't have permissions to speak in your"
+                               " voice channel.")
+            return
+        except ChannelUserLimit:
+            await self.bot.say("Your voice channel is full.")
+            return
+
+        if not self.voice_connected(server):
+            await self._join_voice_channel(voice_channel)
+        else:  # We are connected but not to the right channel
+            if self.voice_client(server).channel != voice_channel:
+                await self._stop_and_disconnect(server)
+                await self._join_voice_channel(voice_channel)
+        
         msg = box("TextToSpeech Enabled")
         self.ttsEnabled = True
         await self.bot.say(msg)
+        
+#    @commands.command(pass_context=True, no_pm=True)
+#    async def connect(self, ctx, *, url_or_search_terms):
 
+    
+    def is_playing(self, server):
+        if not self.voice_connected(server):
+            return False
+        if self.voice_client(server) is None:
+            return False
+        if not hasattr(self.voice_client(server), 'audio_player'):
+            return False
+        if self.voice_client(server).audio_player.is_done():
+            return False
+        return True
+        
+    def voice_connected(self, server):
+        if self.bot.is_voice_connected(server):
+            return True
+        return False
+        
+    
 def setup(bot):
     bot.add_cog(TextToSpeech(bot))
