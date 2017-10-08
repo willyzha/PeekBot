8745a09ca906eda06588bd7257a9fbc72e102f5d
Willy <willyzha@gmail.com>
2017-10-07 23:17:24 -0700

    Add TSS cog

---
+++ cogs/tts.py
@@ -0,0 +1,36 @@
+from discord.ext import commands
+from random import choice
+from .utils.dataIO import dataIO
+from .utils import checks
+from .utils.chat_formatting import box
+from collections import Counter, defaultdict, namedtuple
+import discord
+import time
+import os
+import asyncio
+import chardet
+
+class TextToSpeech:
+    """General commands."""
+    def __init__(self, bot):
+        self.bot = bot
+		self.ttsEnabled = False
+
+	@commands.group(pass_context=True)
+	@checks.mod_or_permission(administrator=True)
+	async def tts(self, ctx):
+		server = ctx.message.server
+		msg = box("TextToSpeech Enabled")
+		self.ttsEnabled = True
+		await self.bot.say(msg)
+		
+	@tss.command(pass_context=True)
+	async def off():
+		"""Turn off TextToSpeech"""
+		server = ctx.message.server
+		msg = box("TextToSpeech Disabled")
+		self.ttsEnabled = False
+		await self.bot.say(msg)
+
+def setup(bot):
+    bot.add_cog(TextToSpeech(bot))
